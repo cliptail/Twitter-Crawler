e88dbf9bae2c3af2c42fad1ac5b56e29a31bef8d
liujianyu <liujianyu@soouya.com>
2024-08-08 15:35:25 +0800

    自动循环，扫描回复

--- main.py
+++ main.py
@@ -47,11 +47,34 @@ if __name__ == '__main__':
             driver.add_cookie(cookie_dict)
         except Exception as e:
             print(cookie['name'], e)
-    print('open url...')
-    driver.get('https://x.com/home')
-    time.sleep(5)
-    if driver.find_elements_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[1]/div[1]/div/nav/div/div[2]/div/div[2]/a'):
-        print("登录成功！")
-    else:
-        print("可能未成功登录")
-    driver.quit()
+    while True:
+        print(time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()))
+        url_list = ['https://x.com/cliptailfish']
+        for url in url_list:
+            print('open url...')
+            driver.get(url)
+            time.sleep(5)
+            print('点击评论')
+            button_xpath = "//section/div/div/div//div[@aria-label]/div[1]/button[@aria-label]"
+            reply_button = driver.find_element(By.XPATH, button_xpath)
+            reply_button.click()
+            time.sleep(3)
+            print('输入评论')
+            # 等待可编辑区域加载完成
+            editable_div_xpath = "//*[@data-testid='tweetTextarea_0']"
+            editable_div = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, editable_div_xpath))
+            )
+            # 聚焦元素
+            editable_div.send_keys(Keys.CONTROL, "a")  # 选中编辑区域内的所有文本
+            editable_div.send_keys(Keys.BACK_SPACE)  # 清除编辑区域内的所有文本
+            # 输入文本
+            tweet_text = "Hello World! This is an auto-filled tweet."
+            editable_div.send_keys(tweet_text)
+            time.sleep(3)
+            print('点击发帖')
+            button_xpath = '//div[@data-testid="toolBar"]/div/button[@data-testid="tweetButton"]'
+            reply_button = driver.find_element(By.XPATH, button_xpath)
+            reply_button.click()
+            time.sleep(5)
+        time.sleep(30)
